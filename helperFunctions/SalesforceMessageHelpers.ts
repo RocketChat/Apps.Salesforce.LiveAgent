b20ea5de1e88dd0d047367626190215511fb1109
Prajval Raval <ravalprajval@gmail.com>
2020-08-15 20:45:56 +0530

    fixing minor typo

--- helperFunctions/SalesforceMessageHelpers.ts
+++ helperFunctions/SalesforceMessageHelpers.ts
@@ -58,7 +58,7 @@ export async function checkForErrorEvents(
 		switch (messageArray.messages[0].message.reason) {
 			case 'Unavailable':
 				console.log(Logs.ERROR_ALL_LIVEAGENTS_UNAVAILABLE);
-				const NoLiveagentAvailableMessage: string = (await this.read.getEnvironmentReader().getSettings().getById('la_no_liveagent_available')).value;
+				const NoLiveagentAvailableMessage: string = (await read.getEnvironmentReader().getSettings().getById('la_no_liveagent_available')).value;
 				await sendLCMessage(modify, message.room, NoLiveagentAvailableMessage, LcAgent);
 				break;
 
