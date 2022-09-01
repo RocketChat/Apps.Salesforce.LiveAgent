ec0eb8bdd97e7f0490fbf0de5029727e225b5ca1
cwsmith <cameron.smith@viasat.com>
2022-09-01 14:27:13 -0400

    Refactor TimeoutHelper file into smaller functions, export app timer reset function

--- helperFunctions/TimeoutHelper.ts
+++ helperFunctions/TimeoutHelper.ts
@@ -1,110 +1,60 @@
 import { IHttp, IModify, IPersistence, IRead } from '@rocket.chat/apps-engine/definition/accessors';
 import { IApp } from '@rocket.chat/apps-engine/definition/IApp';
 import { IMessage } from '@rocket.chat/apps-engine/definition/messages';
+import { RocketChatAssociationRecord } from '@rocket.chat/apps-engine/definition/metadata';
 import { IOnetimeSchedule } from '@rocket.chat/apps-engine/definition/scheduler';
 import { AppSettingId } from '../enum/AppSettingId';
 import { getRoomAssoc, retrievePersistentData, updatePersistentData } from '../helperFunctions/PersistenceHelpers';
 import { getAppSettingValue } from '../lib/Settings';
 
-export const handleTimeout = async (app: IApp, message: IMessage, read: IRead, http: IHttp, persistence: IPersistence, modify: IModify) => {
-	if (message.room.type !== 'l' || (message.customFields && message.customFields.idleTimeoutConfig)) {
-		return;
-	}
+async function updateSneakPeekField(
+	salesforceBotUsername: string,
+	messageId: string,
+	sneakPeekEnabled: boolean | null,
+	read: IRead,
+	modify: IModify,
+) {
+	const user = await read.getUserReader().getByUsername(salesforceBotUsername);
+	const msgExtender = await modify.getExtender().extendMessage(messageId, user);
+	msgExtender.addCustomField('sneakPeekEnabled', sneakPeekEnabled);
+	return modify.getExtender().finish(msgExtender);
+}
 
-	const salesforceBotUsername: string = await getAppSettingValue(read, AppSettingId.SALESFORCE_BOT_USERNAME);
-	const assoc = getRoomAssoc(message.room.id);
-	const { chasitorIdleTimeout, sneakPeekEnabled } = await retrievePersistentData(read, assoc);
-
-	if (chasitorIdleTimeout && chasitorIdleTimeout.isEnabled) {
-		/**
-		 * Sets the amount of time that a customer has to respond to an agent message before a warning appears and a timer begins a countdown.
-		 * The warning disappears (and the timer stops) each time the customer sends a message.
-		 * The warning disappears (and the timer resets to 0) each time the agent sends message.
-		 * The warning value must be shorter than the time-out value (we recommend at least 30 seconds).
-		 */
-		const warningTime = chasitorIdleTimeout.warningTime;
-
-		/**
-		 * Sets the amount of time that a customer has to respond to an agent message before the session ends.
-		 * The timer stops when the customer sends a message and starts again from 0 on the next agent's message.
-		 */
-		const timeoutTime = chasitorIdleTimeout.timeout;
-
-		// ------ When agent sends message -----
-		// Send new timeout msg and reset previous timeout
-
-		// ------ When customer sends message -----
-		// Send timeout msg to cancel previous timeout
-
-		// On Timeout : Close chat
-		// On Warning : Show Countdown Popup in Livechat Widget
-
-		const timeoutWarningMessage: string = await getAppSettingValue(read, AppSettingId.CUSTOMER_TIMEOUT_WARNING_MESSAGE);
-		const sessionTimeoutHandler: string = await getAppSettingValue(read, AppSettingId.TIMEOUT_HANDLER);
-
-		if (message.sender.username === salesforceBotUsername) {
-			// Agent sent message
-			if (!message.id) {
-				return;
-			}
-
-			if (sessionTimeoutHandler === 'app') {
-				await scheduleTimeOut(message, read, modify, persistence, timeoutTime, app, assoc);
-			}
-
-			const user = await read.getUserReader().getByUsername(salesforceBotUsername);
-			const msgExtender = modify.getExtender().extendMessage(message.id, user);
-			(await msgExtender).addCustomField('idleTimeoutConfig', {
-				idleTimeoutAction: 'start',
-				idleTimeoutWarningTime: warningTime,
-				idleTimeoutTimeoutTime: timeoutTime,
-				idleTimeoutMessage: timeoutWarningMessage,
-			});
-			(await msgExtender).addCustomField('sneakPeekEnabled', sneakPeekEnabled);
-			modify.getExtender().finish(await msgExtender);
-		} else {
-			// Guest sent message
-
-			if (!message.id) {
-				return;
-			}
-
-			if (sessionTimeoutHandler === 'app') {
-				await updatePersistentData(read, persistence, assoc, { isIdleSessionTimerScheduled: false, idleSessionTimerId: '' });
-				await modify.getScheduler().cancelJobByDataQuery({ rid: message.room.id, taskType: 'sessionTimeout' });
-			}
-			const user = await read.getUserReader().getByUsername(salesforceBotUsername);
-			const msgExtender = modify.getExtender().extendMessage(message.id, user);
-			(await msgExtender).addCustomField('idleTimeoutConfig', {
-				idleTimeoutAction: 'stop',
-				idleTimeoutWarningTime: warningTime,
-				idleTimeoutTimeoutTime: timeoutTime,
-				idleTimeoutMessage: timeoutWarningMessage,
-			});
-			(await msgExtender).addCustomField('sneakPeekEnabled', sneakPeekEnabled);
-			modify.getExtender().finish(await msgExtender);
-		}
-	} else {
-		if (!message.id) {
-			return;
-		}
-		const user = await read.getUserReader().getByUsername(salesforceBotUsername);
-		const msgExtender = modify.getExtender().extendMessage(message.id, user);
-		(await msgExtender).addCustomField('sneakPeekEnabled', sneakPeekEnabled);
-		modify.getExtender().finish(await msgExtender);
-	}
-};
-
-async function scheduleTimeOut(
+async function modifyWidgetTimer(
+	salesforceBotUsername: string,
 	message: IMessage,
+	chasitorIdleTimeout: any,
+	idleTimeoutAction: string,
+	read: IRead,
+	modify: IModify,
+) {
+	const timeoutWarningMessage: string = await getAppSettingValue(read, AppSettingId.CUSTOMER_TIMEOUT_WARNING_MESSAGE);
+	const warningTime = chasitorIdleTimeout.warningTime;
+	const timeoutTime = chasitorIdleTimeout.timeout;
+
+	const user = await read.getUserReader().getByUsername(salesforceBotUsername);
+	const msgExtender = await modify.getExtender().extendMessage(message.id as string, user);
+	msgExtender.addCustomField('idleTimeoutConfig', {
+		idleTimeoutAction,
+		idleTimeoutWarningTime: warningTime,
+		idleTimeoutTimeoutTime: timeoutTime,
+		idleTimeoutMessage: timeoutWarningMessage,
+	});
+}
+
+async function clearAppTimeout(roomId: string, read: IRead, modify: IModify, persistence: IPersistence, app: IApp, assoc) {
+	await updatePersistentData(read, persistence, assoc, { isIdleSessionTimerScheduled: false, idleSessionTimerId: '' });
+	await modify.getScheduler().cancelJobByDataQuery({ rid: roomId, taskType: 'sessionTimeout' });
+}
+
+async function scheduleAppTimeout(
+	roomId: string,
+	chasitorIdleTimeout: any,
 	read: IRead,
 	modify: IModify,
 	persistence: IPersistence,
-	idleTimeoutTimeoutTime: number,
-	app: IApp,
-	assoc,
+	assoc: RocketChatAssociationRecord,
 ) {
-	const rid = message.room.id;
 	const { isIdleSessionTimerScheduled, idleSessionTimerId } = await retrievePersistentData(read, assoc);
 
 	if (isIdleSessionTimerScheduled === true) {
@@ -115,9 +65,92 @@ async function scheduleTimeOut(
 
 	const task: IOnetimeSchedule = {
 		id: 'idle-session-timeout',
-		when: new Date(new Date().getTime() + idleTimeoutTimeoutTime * 1000),
-		data: { rid, taskType: 'sessionTimeout' },
+		when: new Date(new Date().getTime() + chasitorIdleTimeout.timeout * 1000),
+		data: { rid: roomId, taskType: 'sessionTimeout' },
 	};
 	const jobId = await modify.getScheduler().scheduleOnce(task);
 	await updatePersistentData(read, persistence, assoc, { isIdleSessionTimerScheduled: true, idleSessionTimerId: jobId });
 }
+
+/**
+ * Clears existing app timeout timer, and instantiates a new app timeout timer
+ */
+export async function resetAppTimeout(
+	roomId: string,
+	chasitorIdleTimeout: any,
+	read: IRead,
+	modify: IModify,
+	persistence: IPersistence,
+	app: IApp,
+	assoc: RocketChatAssociationRecord,
+) {
+	const sessionTimeoutHandler: string = await getAppSettingValue(read, AppSettingId.TIMEOUT_HANDLER);
+	if (sessionTimeoutHandler === 'app') {
+		await clearAppTimeout(roomId, read, modify, persistence, app, assoc);
+		await scheduleAppTimeout(roomId, chasitorIdleTimeout, read, modify, persistence, assoc);
+	}
+}
+
+/**
+ * Sets the amount of time that a customer has to respond to an agent message before a warning appears and a timer begins a countdown.
+ *
+ * - The warning disappears (and the timer stops) each time the customer sends a message.
+ * - The warning disappears (and the timer resets to 0) each time the agent sends message.
+ * - The timer stops when the customer sends a message and starts again from 0 on the next agent's message.
+ *
+ * The warning value must be shorter than the time-out value (we recommend at least 30 seconds).
+ */
+export const handleTimeout = async (app: IApp, message: IMessage, read: IRead, http: IHttp, persistence: IPersistence, modify: IModify) => {
+	if (message.customFields?.idleTimeoutConfig) {
+		return;
+	}
+
+	const salesforceBotUsername: string = await getAppSettingValue(read, AppSettingId.SALESFORCE_BOT_USERNAME);
+	const assoc = getRoomAssoc(message.room.id);
+	const { chasitorIdleTimeout, sneakPeekEnabled } = await retrievePersistentData(read, assoc);
+
+	if (chasitorIdleTimeout?.isEnabled) {
+		// ------ When agent sends message -----
+		// Send new timeout msg and reset previous timeout
+
+		// ------ When customer sends message -----
+		// Send timeout msg to cancel previous timeout
+
+		// On Timeout : Close chat
+		// On Warning : Show Countdown Popup in Livechat Widget
+
+		const sessionTimeoutHandler: string = await getAppSettingValue(read, AppSettingId.TIMEOUT_HANDLER);
+
+		if (message.sender.username === salesforceBotUsername) {
+			// Agent sent message
+			if (!message.id) {
+				return;
+			}
+
+			if (sessionTimeoutHandler === 'app') {
+				await scheduleAppTimeout(message.room.id, chasitorIdleTimeout, read, modify, persistence, assoc);
+			}
+
+			await modifyWidgetTimer(salesforceBotUsername, message, chasitorIdleTimeout, 'start', read, modify);
+			await updateSneakPeekField(salesforceBotUsername, message.room.id, sneakPeekEnabled, read, modify);
+		} else {
+			// Guest sent message
+			if (!message.id) {
+				return;
+			}
+
+			if (sessionTimeoutHandler === 'app') {
+				await clearAppTimeout(message.room.id, read, modify, persistence, app, assoc);
+			}
+
+			await modifyWidgetTimer(salesforceBotUsername, message, chasitorIdleTimeout, 'stop', read, modify);
+			await updateSneakPeekField(salesforceBotUsername, message.room.id, sneakPeekEnabled, read, modify);
+		}
+	} else {
+		if (!message.id) {
+			return;
+		}
+
+		await updateSneakPeekField(salesforceBotUsername, message.id, sneakPeekEnabled, read, modify);
+	}
+};
